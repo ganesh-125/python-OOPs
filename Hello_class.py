2bcc407b2a2588fe8d40d82c7ff6a82203bd882f
Ganesh Prajapati <ganeshprajapati@Ganeshs-MacBook-Pro.local>
2021-07-14 08:01:29 +0530

    Simple class defination

---
+++ Hello_class.py
@@ -0,0 +1,8 @@
+''' Simple Class defination'''
+
+class HelloClass:
+    # __init__ method automatically called when a instance of class is created
+    def __init__(self):
+        print("Hello, Class")
+
+obj1 = HelloClass()
